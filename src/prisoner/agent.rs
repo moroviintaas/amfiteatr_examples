04efd7073441a383db6dc1c2452143a384f978a3
moroviintaas <moroviintaas@gmail.com>
2023-07-31 20:50:54 +0200

    Implementing tensor conversions

--- src/prisoner/agent.rs
+++ src/prisoner/agent.rs
@@ -1,9 +1,12 @@
 use std::cell::Cell;
 use std::fmt::{Display, Formatter};
 use rand::seq::IteratorRandom;
+use tch::Tensor;
 use sztorm::agent::Policy;
+use sztorm::error::ConvertError;
 use sztorm::Reward;
 use sztorm::state::agent::{InformationSet, ScoringInformationSet};
+use sztorm_rl::tensor_repr::{ActionTensor, ConvStateToTensor};
 use crate::prisoner::common::RewardTable;
 use crate::prisoner::domain::{PrisonerAction, PrisonerDomain, PrisonerError, PrisonerUpdate};
 use crate::prisoner::domain::PrisonerAction::{Betray, Cover};
@@ -12,18 +15,23 @@ use crate::prisoner::domain::PrisonerAction::{Betray, Cover};
 pub struct PrisonerState{
     previous_actions: Vec<PrisonerUpdate>,
     reward_table: RewardTable,
-    last_action: Cell<Option<PrisonerAction>>
+    //last_action: Cell<Option<PrisonerAction>>
 
 }
 
 impl PrisonerState{
     pub fn new(reward_table: RewardTable) -> Self{
-        Self{reward_table, last_action: Cell::new(None), previous_actions: Vec::new()}
+        Self{
+            reward_table,
+            //last_action: Cell::new(None),
+            previous_actions: Vec::new()}
     }
 
-    pub fn _select_action(&self, action: PrisonerAction){
+    /*pub fn _select_action(&self, action: PrisonerAction){
         self.last_action.set(Some(action));
     }
+
+     */
 
     pub fn previous_actions(&self) -> &Vec<PrisonerUpdate>{
         &self.previous_actions
@@ -54,7 +62,7 @@ impl Policy<PrisonerDomain> for CoverPolicy{
     type StateType = PrisonerState;
 
     fn select_action(&self, state: &Self::StateType) -> Option<PrisonerAction> {
-        state._select_action(Cover);
+        //state._select_action(Cover);
         Some(Cover)
     }
 }
@@ -69,10 +77,10 @@ impl Policy<PrisonerDomain> for Forgive1Policy{
             step.other_prisoner_action == Betray
         }).count();
         if enemy_betrayals > 1 {
-            state._select_action(Betray);
+            //state._select_action(Betray);
             Some(Betray)
         } else {
-            state._select_action(Cover);
+            //state._select_action(Cover);
             Some(Cover)
         }
 
@@ -93,10 +101,10 @@ impl Policy<PrisonerDomain> for BetrayRatioPolicy{
             .count();
 
         if betrayed > covered{
-            state._select_action(Betray);
+            //state._select_action(Betray);
             Some(Betray)
         } else {
-            state._select_action(Cover);
+            //state._select_action(Cover);
             Some(Cover)
         }
     }
@@ -113,7 +121,7 @@ impl Policy<PrisonerDomain> for RandomPrisonerPolicy{
     fn select_action(&self, state: &Self::StateType) -> Option<PrisonerAction> {
         let mut rng = rand::thread_rng();
         state.available_actions().into_iter().choose(&mut rng).and_then(|a|{
-            state._select_action(a);
+            //state._select_action(a);
             Some(a)
         })
 
@@ -136,8 +144,11 @@ impl InformationSet<PrisonerDomain> for PrisonerState{
     }
 
     fn update(&mut self, update: PrisonerUpdate) -> Result<(), PrisonerError> {
+        /*
         let last = self.last_action.get();
+
         if let Some(my_action) = last{
+
             if my_action == update.own_action{
                 self.previous_actions.push(update);
                 self.last_action.set(None);
@@ -145,9 +156,14 @@ impl InformationSet<PrisonerDomain> for PrisonerState{
             } else{
                 Err(PrisonerError::DifferentActionPerformed {chosen: my_action, logged: update.own_action})
             }
+
         } else {
             Err(PrisonerError::NoLastAction(update.own_action))
         }
+
+         */
+        self.previous_actions.push(update);
+        Ok(())
     }
 }
 
@@ -175,3 +191,58 @@ impl ScoringInformationSet<PrisonerDomain> for PrisonerState{
     }
 }
 
+pub struct PrisonerStateTranslate{
+
+}
+
+impl ConvStateToTensor<PrisonerState> for PrisonerStateTranslate{
+    fn make_tensor(&self, t: &PrisonerState) -> Tensor {
+        let mut array = [0.0;2*256];
+        for i in 0..t.previous_actions().len(){
+            array[2*i] = match t.previous_actions()[i].own_action{
+                Betray =>  1.0,
+                Cover => 2.0,
+            };
+            array[2*i+1] = match t.previous_actions()[i].other_prisoner_action{
+                Betray =>  1.0,
+                Cover => 2.0,
+            };
+        }
+        Tensor::from_slice(&array[..])
+    }
+}
+
+impl ActionTensor for PrisonerAction{
+    fn to_tensor(&self) -> Tensor {
+        match self{
+            Betray => Tensor::from_slice(&[1.0f64;1]),
+            Cover => Tensor::from_slice(&[2.0f64;1])
+        }
+    }
+
+
+    /// ```
+    /// use tch::Tensor;
+    /// use sztorm_examples::prisoner::domain::PrisonerAction;
+    /// use sztorm_examples::prisoner::domain::PrisonerAction::{Betray, Cover};
+    /// use sztorm_rl::tensor_repr::ActionTensor;
+    /// let t = Tensor::from_slice(&[1i64;1]);
+    /// let action = PrisonerAction::try_from_tensor(&t).unwrap();
+    /// assert_eq!(action, Betray);
+    /// ```
+    fn try_from_tensor(t: &Tensor) -> Result<Self, ConvertError> {
+
+
+        let v: Vec<i64> = match Vec::try_from(t){
+            Ok(v) => v,
+            Err(e) =>{
+                return Err(ConvertError::ActionDeserialize(format!("{}", t)))
+            }
+        };
+        match v[0]{
+            1 => Ok(Betray),
+            2 => Ok(Cover),
+            _ => Err(ConvertError::ActionDeserialize(format!("{}", t)))
+        }
+    }
+}

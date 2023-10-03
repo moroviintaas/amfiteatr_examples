d3f2e45326f832d99d15843ca89655615682dfff
moroviintaas <moroviintaas@gmail.com>
2023-10-03 18:45:22 +0200

    Sztorm's move Action, Reward to module domain

--- src/prisoner/domain.rs
+++ src/prisoner/domain.rs
@@ -1,9 +1,8 @@
 use std::fmt::{Display, Formatter};
 use std::ops::{Index, IndexMut};
-use sztorm::Action;
 use sztorm::agent::{AgentIdentifier};
 use sztorm::error::{InternalGameError, SztormError};
-use sztorm::domain::DomainParameters;
+use sztorm::domain::{Action, DomainParameters};
 use sztorm::state::StateUpdate;
 use crate::prisoner::domain::PrisonerId::{Andrzej, Janusz};
 
